ea77116147aa248bdb38a231fb3b991d98508788
Edson Vladimir <edson.vladimir.sosa@gmail.com>
2023-12-04 21:14:16 -0400

    se añadio el decorador

---
+++ src/patterns/state/State.java
@@ -0,0 +1,13 @@
+package patterns.state;
+
+public abstract class State {
+    Player player;
+    State(Player player) {
+        this.player = player;
+    }
+
+    public abstract String onLock();
+    public abstract String onPlay();
+    public abstract String onNext();
+    public abstract String onPrevious();
+}
